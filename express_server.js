ae7c15bd8a58a8d92acb2a71df85ccc81ca5a76a
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-05 16:32:56 +0000

    Add urls_show.ejs and routing to that template in express_server.js

--- express_server.js
+++ express_server.js
@@ -26,6 +26,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+  } else {
+    res.statusCode = 404;
+    res.send("<html><body><h1>404 Does Not Exist</h1><h3>This Tiny URL does not exists</h3></body></html>");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
