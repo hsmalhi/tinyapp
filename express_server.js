88fba39acc3317855dbd1ee28ab9972e2002a6f2
Harjot Singh Malhi <hsmalhi94@hotmail.com>
2019-08-05 14:09:31 +0000

    Add /urls template and send urls data to template

--- express_server.js
+++ express_server.js
@@ -13,12 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
